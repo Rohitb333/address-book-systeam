2564233390b5d727e9a1fe7746c355d491a41b81
Rohit <rohitb333@gmail.com>
2022-03-16 20:39:42 +0530

    added edit contact file

--- src/com/bridelabz/AddressBookMain.java
+++ src/com/bridelabz/AddressBookMain.java
@@ -7,38 +7,80 @@ public class AddressBookMain {
         System.out.println("Welcome to Address book Program !");
         ContactPerson person = new ContactPerson();
         List<ContactPerson> contact = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Address Book Menu");
-        System.out.println("1. Add New Person address");
-        int choice = Integer.parseInt(sc.nextLine());
-        switch (choice){
-            case 1:
-                System.out.println("First name");
-                person.setFirst_name(sc.nextLine());
-                System.out.println("Last Name");
-                person.setLast_name(sc.nextLine());
-                System.out.println("Address");
-                person.setAddress(sc.nextLine());
-                System.out.println("City");
-                person.setCity(sc.nextLine());
-                System.out.println("State");
-                person.setState(sc.nextLine());
-                System.out.println("Zip");
-                person.setZip(Integer.parseInt(sc.nextLine()));
-                System.out.println("PhoneNo");
-                person.setPhone_number((sc.nextLong()));
-                System.out.println("Email");
-                person.setEmail(sc.nextLine());
+        try {
+            Scanner sc = new Scanner(System.in);
+            boolean isAlive = true;
+            do {
+                System.out.println("Address Book Menu");
+                System.out.println("1. Add New Person address");
+                System.out.println("2. Update Person address");
+                System.out.println("3. Exit for Address Book");
+                int choice = Integer.parseInt(sc.nextLine());
+                boolean ans = contact.isEmpty();
 
-                contact.add(new ContactPerson(person.getFirst_name(), person.getLast_name(), person.getAddress(), person.getCity(),
-                        person.getState(), person.getZip(), person.getPhone_number(), person.getEmail()));
-                System.out.println(contact);
-                System.out.println("New address added to the book successfully !");
-                break;
-            default:
-                System.out.println("Please enter valid choice ");
-                break;
+                switch (choice) {
+                    case 1:
+                        System.out.println("First name");
+                        person.setFirst_name(sc.nextLine());
+                        System.out.println("Last Name");
+                        person.setLast_name(sc.nextLine());
+                        System.out.println("Address");
+                        person.setAddress(sc.nextLine());
+                        System.out.println("City");
+                        person.setCity(sc.nextLine());
+                        System.out.println("State");
+                        person.setState(sc.nextLine());
+                        System.out.println("Zip");
+                        person.setZip(Integer.parseInt(sc.nextLine()));
+                        System.out.println("PhoneNo");
+                        person.setPhone_number((sc.nextLong()));
+                        System.out.println("Email");
+                        person.setEmail(sc.nextLine());
+
+                        contact.add(new ContactPerson(person.getFirst_name(), person.getLast_name(), person.getAddress(), person.getCity(),
+                                person.getState(), person.getZip(), person.getPhone_number(), person.getEmail()));
+                        System.out.println(contact);
+                        System.out.println("New address added to the book successfully !");
+                        break;
+                    case 2:
+                        if (ans == true ){
+                            System.out.println("Address Book is Empty. Please enter atleast one entry to update.");
+                        }else {
+                            System.out.println("Enter entry index(i.e. 0) to update ?");
+                            int etr = Integer.parseInt(sc.nextLine());
+                            System.out.println("Update First name");
+                            person.setFirst_name(sc.nextLine());
+                            System.out.println("Update Last Name6");
+                            person.setLast_name(sc.nextLine());
+                            System.out.println("Update Address");
+                            person.setAddress(sc.nextLine());
+                            System.out.println("Update City");
+                            person.setCity(sc.nextLine());
+                            System.out.println("Update State");
+                            person.setState(sc.nextLine());
+                            System.out.println("Update Zip");
+                            person.setZip(Integer.parseInt(sc.nextLine()));
+                            System.out.println("Update PhoneNo");
+                            person.setPhone_number(sc.nextLong());
+                            System.out.println("Update Email");
+                            person.setEmail(sc.nextLine());
+                            contact.set(etr, person);
+                            System.out.println("Updated AddressBook : " + contact.get(etr));
+                            System.out.println("Address Book Updated Successfully !");
+                        }
+                        break;
+                    case 3:
+                        isAlive = false;
+                        System.out.println("You have chosen Exit option .");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Please enter valid choice ");
+                        break;
+                }
+            } while (isAlive != false);
+        }catch (Exception e){
+            System.out.println("Please enter valid inputs. Re-execute the program and ensure that your enter Zip and Phone Number as integer and all other input must be string");
         }
-
     }
 }
