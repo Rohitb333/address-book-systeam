517b9d24bbc1b9975ab904904cb58056ecfa474d
Rohit <rohitb333@gmail.com>
2022-03-16 20:22:45 +0530

    added create contact file

---
+++ src/com/bridelabz/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book Program !");
+        ContactPerson person = new ContactPerson();
+    }
+}
